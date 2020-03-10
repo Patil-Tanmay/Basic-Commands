b6be01a9c5574668c1ddf95c7fbcaca36c8c1e2c
tanmay7715 <47470943+tanmay7715@users.noreply.github.com>
2020-03-10 13:05:35 +0530

    First commit

---
+++ BasicCommands/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace BasicCommands
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.File = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Move = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.FIle_delete = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(327, 39);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Browse File";
+            // 
+            // File
+            // 
+            this.File.Location = new System.Drawing.Point(6, 78);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(26, 24);
+            this.File.TabIndex = 3;
+            this.File.Text = "...";
+            this.File.UseVisualStyleBackColor = true;
+            this.File.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(6, 107);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(75, 30);
+            this.Copy.TabIndex = 4;
+            this.Copy.Text = "Copy To";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 194);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(324, 28);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(293, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0%";
+            // 
+            // Move
+            // 
+            this.Move.Location = new System.Drawing.Point(121, 107);
+            this.Move.Name = "Move";
+            this.Move.Size = new System.Drawing.Size(73, 30);
+            this.Move.TabIndex = 7;
+            this.Move.Text = "move to";
+            this.Move.UseVisualStyleBackColor = true;
+            this.Move.Click += new System.EventHandler(this.Move_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(341, 261);
+            this.tabControl1.TabIndex = 8;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.FIle_delete);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.Move);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.File);
+            this.tabPage1.Controls.Add(this.Copy);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(333, 232);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Basic Functions";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(333, 253);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Powershell ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // FIle_delete
+            // 
+            this.FIle_delete.Location = new System.Drawing.Point(243, 107);
+            this.FIle_delete.Name = "FIle_delete";
+            this.FIle_delete.Size = new System.Drawing.Size(69, 30);
+            this.FIle_delete.TabIndex = 8;
+            this.FIle_delete.Text = "Delete";
+            this.FIle_delete.UseVisualStyleBackColor = true;
+            this.FIle_delete.Click += new System.EventHandler(this.FIle_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 261);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Basic Commands";
+            this.MaximumSizeChanged += new System.EventHandler(this.Form1_MaximumSizeChanged);
+            this.MinimumSizeChanged += new System.EventHandler(this.Form1_MinimumSizeChanged);
+            this.Load += new System.EventHandler(this.Form1_Load);
+          
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button File;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Move;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button FIle_delete;
+    }
+}
+
---
+++ BasicCommands/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace BasicCommands
+{
+    public partial class Form1 : Form   
+    {   
+        
+        BackgroundWorker _copyWorker = new BackgroundWorker();
+        BackgroundWorker _moveWorker = new BackgroundWorker();
+        BackgroundWorker _deleteworker = new BackgroundWorker();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _deleteworker.WorkerSupportsCancellation = true;
+            _deleteworker.WorkerReportsProgress = true;
+            _deleteworker.ProgressChanged += worker_progresschanged;
+
+            _copyWorker.WorkerSupportsCancellation = true;
+            _moveWorker.WorkerSupportsCancellation = true;
+
+            _copyWorker.WorkerReportsProgress = true;
+            _moveWorker.WorkerReportsProgress = true;
+
+            _copyWorker.ProgressChanged += worker_progresschanged;
+            _moveWorker.ProgressChanged += worker_progresschanged;
+
+           
+            _copyWorker.DoWork += copywork;
+            _moveWorker.DoWork += movework;
+            _deleteworker.DoWork += deletework;
+        }
+        //used in copy_click button
+        string copy_s;
+        //used in move click button
+        string move_s;
+        //calling move function inn here so that value of progrssbar gets changed accordingly 
+        private void deletework(object sender,DoWorkEventArgs e)
+        {
+            delete_f(textBox1.Text);
+        }
+        private void movework(object sender,DoWorkEventArgs e)
+        {
+            move(textBox1.Text,move_s);
+        }
+        //calling copy function in do work so that value of progressbar gets changed accordingly
+        private void copywork(object sender, DoWorkEventArgs e)
+        {
+            copy_p(textBox1.Text,copy_s);
+            
+        }
+        //progressbar function
+        private void worker_progresschanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+           
+            label2.Text = progressBar1.Value.ToString() +"%";
+            if (progressBar1.Value==100) {
+                progressBar1.Value = 0;
+                label2.Text = progressBar1.Value + "%";
+            }
+        }
+
+        string path;
+     
+        //file selection
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.RestoreDirectory = true;
+            fd.Filter = "All Files(*.*)|*.*";
+            fd.Title = "Select a File";
+            fd.FileName = "Any File";
+            
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                 path = fd.FileName;               
+            }
+            textBox1.Text = path;
+
+
+        }
+        string temp;
+        //to execute cmd commands
+        private static string Execute(string Startfunc, String Arguments, String SetWorkingdir = null)
+        {
+           
+            string text = null;
+            Process p = new Process();
+            p.StartInfo.FileName = Startfunc;
+            p.StartInfo.Arguments = Arguments;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            if (SetWorkingdir != null)
+                p.StartInfo.WorkingDirectory = SetWorkingdir;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.EnableRaisingEvents = true;
+            p.Start();
+            do
+            {
+                Application.DoEvents();
+                string output = p.StandardOutput.ReadToEnd();
+                text += output;
+                string err = p.StandardError.ReadToEnd();
+                text += err;
+            }
+            while (!p.HasExited);
+            return text;
+        }
+        //copy paste funtion
+        public void copy_p(string source,string destination)
+        {
+            FileStream fsout = new FileStream(destination,FileMode.Create);
+            FileStream fsin = new FileStream(source, FileMode.Open);
+            byte[] bt = new byte[1047856];
+            int readbyte;
+            while((readbyte=fsin.Read(bt ,0, bt.Length)) > 0)
+            {
+                fsout.Write(bt, 0, readbyte);
+                _copyWorker.ReportProgress((int)(fsin.Position*100/fsin.Length));
+            }
+            fsin.Close();
+            fsout.Close();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fdb = new FolderBrowserDialog();
+            if (fdb.ShowDialog()==DialogResult.OK)
+            {
+              copy_s=  Path.Combine(fdb.SelectedPath,Path.GetFileName(textBox1.Text));
+                _copyWorker.RunWorkerAsync();
+            }
+       
+      
+        }
+        //funtion to move file
+        public void move(string source,string des)
+        {
+
+            copy_p(source, des);
+            if (System.IO.File.Exists(source))
+            {
+                System.IO.File.Delete(source);
+            }
+        }
+
+        private void Move_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fdb = new FolderBrowserDialog();
+            if (fdb.ShowDialog() == DialogResult.OK)
+            {   //very good way to get an destination path
+                move_s = Path.Combine(fdb.SelectedPath, Path.GetFileName(textBox1.Text));
+                MessageBox.Show(""+move_s);
+                _moveWorker.RunWorkerAsync();
+
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_MaximumSizeChanged(object sender, EventArgs e)
+        {
+            this.MaximumSize = new System.Drawing.Size(464, 454);
+        }
+
+        private void Form1_MinimumSizeChanged(object sender, EventArgs e)
+        {
+            this.MinimumSize = new System.Drawing.Size(361, 305);
+        }
+       //resizing the form size according to my conditions
+       //firat i set the tabcontrol's property to dock then i changed the form size 
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            if (tabControl1.SelectedTab == tabPage2)
+            {
+                this.Width= this.Width + 100;
+                this.Height= this.Height + 100;
+            }
+            else
+            {
+                this.Width = this.Width - 100;
+                this.Height = this.Height - 100;
+            }
+            
+
+        }
+        public void delete_f(string source)
+        {
+            FileInfo fi = new FileInfo(textBox1.Text);
+            long size = fi.Length;
+            System.IO.File.Delete(textBox1.Text);
+                
+            
+
+        }
+        private void FIle_delete_Click(object sender, EventArgs e)
+        {
+            //if (System.IO.File.Exists(textBox1.Text))
+            // {
+            //  System.IO.File.Delete(textBox1.Text);
+            //}
+            _deleteworker.RunWorkerAsync();
+           
+        }
+    }
+}
